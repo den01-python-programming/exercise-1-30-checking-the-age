45261991e53dcb50f5ab912506fdab9660a40a04
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 09:16:04 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,38 @@
 import pytest
-from src.exercise import main
+import src.exercise
 
-def test_exercise(capsys):
-    main()
-    out, err = capsys.readouterr()
-    assert out == "\n", "Should read ''"
+def test_exercise():
+    input_values = ["5","25","-2","135"]
+    input_values_stored = ["5","25","-2","135"]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert [output[0],output[1],output[2],output[3]] == ["How old are you?","OK","How old are you?","OK"]
+    assert [output[4],output[5],output[6],output[7]] == ["How old are you?","Impossible!","How old are you?","Impossible!"]
